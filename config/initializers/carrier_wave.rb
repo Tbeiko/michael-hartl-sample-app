da62380810f5ae7908534eb6a778a537dd4e2e3b
Tim <t.beiko23@gmail.com>
2014-11-07 08:47:21 +0000

    fix s3 keys

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -3,9 +3,9 @@ if Rails.env.production?
     config.fog_credentials = {
       # Configuration for Amazon S3
       :provider              => 'AWS',
-      :aws_access_key_id     => ENV['AKIAJYVZUP2B42BITUKA'],
-      :aws_secret_access_key => ENV['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']
+      :aws_access_key_id     => ENV['S3_ACCESS_KEY'],
+      :aws_secret_access_key => ENV['S3_SECRET_KEY']
     }
-    config.fog_directory     =  ENV['sampletwitter']
+    config.fog_directory     =  ENV['S3_BUCKET']
   end
 end
